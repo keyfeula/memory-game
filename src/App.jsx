916d68569280361072755393ed6ac042f03125a2
Key Feula <keyfeula1214@gmail.com>
2025-11-28 20:09:15 -0700

    Refactor Card.jsx and App.jsx
    Took API logic out of Card component in favor of calling each API at once in App component, then rendering each card using the API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Card } from './components/Card';
 import './App.css';
 
 function App() {
-  const [pkmnList, setPkmnList] = useState([]);
-  const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
-  const pkmnIDs = [1, 4, 7, 152, 155, 158, 252, 255, 258, 387, 390, 393];
+    const [pkmnList, setPkmnList] = useState([]);
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    const pkmnIDs = [1, 4, 7, 152, 155, 158, 252, 255, 258, 387, 390, 393];
 
-  function createCards() {
-    return pkmnIDs.map((idNum, index) => <Card id={idNum} key={index}></Card>)
-  }
+    function shuffle() {
+        
+    }
 
-  return (
-    <>
-      <header>
+    useEffect(() => {
+        async function getData() {
+            const pkmnData = [];
+            for (const id of pkmnIDs) {
+                try {
+                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
-      </header>
-      <main>
-        <ul className='cardList'>{createCards()}</ul>
-      </main>
-    </>
-  )
+                    if (!response.ok) {
+                        throw new Error("Error status: " + response.status);
+                    }
+
+                    const data = await response.json();
+
+                    pkmnData.push({id, name: data.name, img: data.sprites.front_default});
+                    setPkmnList([...pkmnData]);
+                }
+                catch(error) {
+                    console.log(error.message);
+                }
+            }
+        }
+
+        getData();
+    }, [])
+
+    return (
+        <>
+            <header>
+
+            </header>
+            <main>
+                <ul className='cardList'>
+                    {pkmnList.map(({name, img, id}) => <Card key={id} name={name} img={img}></Card>)}
+                </ul>
+            </main>
+        </>
+    )
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,17 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import "../styles/Card.css";
 
-export function Card({id}) {
-
-    const [pkmnData, setPkmnData] = useState({});
-
-    useEffect(() => {
-        async function getData() {
-            try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-                if (!response.ok) {
-                    throw new Error("Error status: " + response.status);
-                }
-
-                const data = await response.json();
-
-                setPkmnData({name: data.name, img: data.sprites.front_default});
-            }
-            catch(error) {
-                console.log(error.message);
-            }
-        }
-        getData();
-    }, [])
+export function Card({name, img, onClick}) {
 
     return (
         <li 
             className="card" 
+            onClick={onClick}
             tabIndex={0}
             role="button"
         >
             <figure>
-                <img src={pkmnData.img} alt={pkmnData.name} />
-                <figcaption>{pkmnData.name}</figcaption>
+                <img src={img} alt={name} />
+                <figcaption>{name}</figcaption>
             </figure>
         </li>
     )
